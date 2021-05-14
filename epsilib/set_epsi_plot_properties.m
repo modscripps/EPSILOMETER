4f8f023f293fdf91105320389d41158da0f4f62b
ncouto <ncouto@ucsd.edu>
2021-05-14 13:31:26 -0700

    NC epsi_class set_epsi_plot_properties
    epsi_class
    l.30 - added check to see if Meta_Data contains paths available on this machine. Otherwise, create new Meta_Data
    l.85 - created subfunction to load default plot properties
    
    set_epsi_plot_properties - new function to load default plot properties
    
    plot_profile_and_spectra
    l.64 - calls set_epsi_plot_properties

---
+++ epsilib/set_epsi_plot_properties.m
@@ -0,0 +1,15 @@
+function [plot_properties] = set_epsi_plot_properties()            
+plot_properties.FontName='Sans';
+            plot_properties.FontSize=16;
+            plot_properties.LineWidth=2;
+            plot_properties.Colors.a1 = [129 27 112]./255;
+            plot_properties.Colors.a2 = [235 64 61]./255;
+            plot_properties.Colors.a3 = [245 199 118]./255;
+            plot_properties.Colors.s1 = [60 134 76]./255;
+            plot_properties.Colors.s2 = [173 215 136]./255;
+            plot_properties.Colors.t1 = [29 78 140]./255;
+            plot_properties.Colors.t2 = [78 173 173]./255;
+            plot_properties.Colors.P = [0.2353    0.2157    0.5098];
+            plot_properties.Colors.dPdt = [0.4851    0.4644    0.7140];
+            plot_properties.Colors.T = [185 38 26]./255;
+            plot_properties.Colors.S = [0 0 0];
