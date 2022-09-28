b5a44647f10de0422d181ea01bfddbaf958e3236
ncouto <ncouto@ucsd.edu>
2022-09-28 16:47:21 -0700

    set_paths scripts

--- set_paths_for_processing.m
+++ set_paths_for_processing.m
@@ -1,13 +1,31 @@
+% I originally created this because I was developing a separate Epsi GUI
+% from the FastCTD GUI. The FastCTD GUI uses a bunch of file names that
+% call each other and I didn't want to mess with that, but I did want to
+% allow the Epsi GUI to plot different variables, so I made two folders
+% with the same file names inside. Matlab gets confused and points to the
+% wrong files if you have duplicate names like this. In order to switch
+% between the two GUI systems to process and plot either Epsi or FastCTD 
+% data, I made these 'set_path' scripts to remove one or the other
+% directory from the search path.
+%
+% LONG STORY SHORT, I didn't finish the GUI and never needed this switching
+% but I DID like changing the color of my command window for realtime
+% visualization vs processing, and for the FastCTD twist counter (which
+% does need a different set of paths).
+% 
+% Nicole Couto | post-BLT3, 2022
+% -------------------------------------------------------------------------
+
 % Add Epsi library
 startup
 Epsi_library = '/Volumes/FCTD Softwares used in BLT 2022/EPSILOMETER_FCTD/';
 addpath(genpath(Epsi_library));
 
 % Remove archived scripts
-rmpath(genpath(fullfile(Epsi_library,'archived_scripts')));
+%rmpath(genpath(fullfile(Epsi_library,'archived_scripts')));
 
 % Remove realtime_fctd scripts
-rmpath(genpath(fullfile(Epsi_library,'realtime','realtime_epsi')));
+%rmpath(genpath(fullfile(Epsi_library,'realtime','realtime_epsi')));
 
 % Set color to cyan
 %get properties
